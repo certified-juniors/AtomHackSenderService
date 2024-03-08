6c29e55759368429b3d62fbac291b0672e8284b2
SicParv1sMagna <92353726+SicParv1sMagna@users.noreply.github.com>
2024-03-08 04:30:25 +0300

    updated minio conn

--- internal/config/minio.go
+++ internal/config/minio.go
@@ -1,7 +1,7 @@
 package config
 
 type Minio struct {
-	Endpoint      string `env:"MARS_MINIO_ENDPOINT"`
+	Endpoint      string `env:"MARS_MINIO_URL"`
 	MinioUser     string `env:"MARS_MINIO_ROOT_USER"`
 	MinioPassword string `env:"MARS_MINIO_ROOT_PASSWORD"`
 	MinioBucket   string `env:"MARS_MINIO_BUCKET"`
