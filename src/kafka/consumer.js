6047b4b4a13041bf7f13cc84f12e99ba872ad947
SicParv1sMagna <92353726+SicParv1sMagna@users.noreply.github.com>
2024-03-10 16:14:22 +0300

    updated logic of size calculating

--- src/kafka/consumer.js
+++ src/kafka/consumer.js
@@ -2,6 +2,7 @@ const { kafka } = require('./kafka');
 const { Message } = require('../message');
 const { periods } = require('../period');
 const { downloadFilesFromBucket } = require('../storage/minio');
+const { calculateFileSize } = require('../utils/helpers');
 
 let consumer;
 
@@ -17,6 +18,8 @@ const runNewConsumer = async () => {
             const msg = new Message(message.value.toString());
             msg.files = await downloadFilesFromBucket(msg.files);
             console.log('message files', msg.files);
+            msg.size = calculateFileSize(message.value.toString());
+            console.log("message size: ", msg.size);
             msg.handle();
         },
     });
